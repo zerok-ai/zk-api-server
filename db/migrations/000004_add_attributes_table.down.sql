90a312f7192865998dd2983b76918f76adea2c15
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2023-10-10 14:23:57 +0530

    add db migrations

--- db/migrations/000004_add_attributes_table.down.sql
+++ db/migrations/000004_add_attributes_table.down.sql
@@ -1,1 +1,5 @@
+DROP TRIGGER IF EXISTS update_attributes_updated_at ON attributes;
+
+DROP FUNCTION IF EXISTS update_updated_at();
+
 drop TABLE attributes;
--- db/migrations/000004_add_attributes_table.up.sql
+++ db/migrations/000004_add_attributes_table.up.sql
@@ -8,3 +8,21 @@ CREATE TABLE IF NOT EXISTS attributes
     updated_at     BIGINT       NOT NULL DEFAULT EXTRACT(EPOCH FROM NOW()),
     CONSTRAINT unique_version_key_set UNIQUE (version, protocol, executor)
 );
+
+CREATE OR REPLACE FUNCTION update_updated_at()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = EXTRACT(EPOCH FROM NOW());
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_attributes_updated_at
+    BEFORE UPDATE ON attributes
+    FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+CREATE TRIGGER update_scenarios_updated_at
+    BEFORE UPDATE ON scenario
+    FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
