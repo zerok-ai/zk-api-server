56d5f719cf0469fc754e30c077adec7e13f2220b
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2023-10-31 14:41:12 +0530

    fix query

--- app/scenario/repository/scenarioRepo.go
+++ app/scenario/repository/scenarioRepo.go
@@ -16,7 +16,7 @@ const (
 	DefaultClusterId = "Zk_default_cluster_id_for_all_scenarios"
 
 	GetAllScenarioSqlStatement             = "SELECT scenario_data, deleted, disabled, created_at, disabled_at, updated_at FROM zk_scenario s INNER JOIN zk_scenario_version sv USING(scenario_id) WHERE updated_at>$1 AND (cluster_id=$2 OR cluster_id=$3) order by created_at desc LIMIT $4 OFFSET $5"
-	GetScenarioByIdSqlStatement            = "SELECT scenario_data, deleted, disabled, created_at, disabled_at, updated_at FROM zk_scenario s INNER JOIN zk_scenario_version sv USING(scenario_id) WHERE scenario_id>$1 AND (cluster_id=$2 OR cluster_id=$3)"
+	GetScenarioByIdSqlStatement            = "SELECT scenario_data, deleted, disabled, created_at, disabled_at, updated_at FROM zk_scenario s INNER JOIN zk_scenario_version sv USING(scenario_id) WHERE scenario_id=$1 AND (cluster_id=$2 OR cluster_id=$3)"
 	GetAllScenarioForDashboardSqlStatement = "SELECT scenario_data, deleted, disabled, created_at, disabled_at, updated_at FROM zk_scenario s INNER JOIN zk_scenario_version sv USING(scenario_id) WHERE updated_at>$1 AND deleted = $2 AND (cluster_id=$3 OR cluster_id=$4) order by created_at desc LIMIT $5 OFFSET $6"
 	InsertScenarioTableStatement           = "INSERT INTO zk_scenario (cluster_id, scenario_title, scenario_type, updated_at) VALUES ($1, $2, $3, $4) RETURNING scenario_id"
 	InsertScenarioVersionTableStatement    = "INSERT INTO zk_scenario_version (scenario_id, scenario_data, schema_version, scenario_version, created_by, created_at) VALUES ($1, $2, $3, $4, $5, $6)"
