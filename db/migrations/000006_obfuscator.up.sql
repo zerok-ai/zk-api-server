f02abd205fd202da1edd5a07f2ee71c094ba6c4a
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-11-13 11:52:19 +0530

    Added migration changes for obfuscation table

---
+++ db/migrations/000006_obfuscator.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.zk_obfuscation (
+   id uuid NOT NULL DEFAULT uuid_generate_v4(),
+   org_id character varying(255) NOT NULL,
+   rule_name character varying(255) NOT NULL,
+   rule_type character varying(255) NOT NULL,
+   rule_def bytea,
+   created_at timestamp without time zone NOT NULL,
+   updated_at timestamp without time zone NOT NULL,
+   deleted boolean DEFAULT false,
+   disabled boolean DEFAULT false,
+   PRIMARY KEY (id)
+);
