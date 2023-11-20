72c22b876de4adcfeb614d277fc81ceeb291ae37
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2023-11-20 14:44:18 +0530

    add endpoint

--- app/routes.go
+++ app/routes.go
@@ -28,6 +28,7 @@ func Initialize(app router.Party, rh scenarioHandler.ScenarioHandler, ch cluster
 		clusterAPI.Post("/{"+utils.ClusterIdxPathParam+"}/integration", ih.UpsertIntegration)
 		clusterAPI.Get("/{"+utils.ClusterIdxPathParam+"}/integration", ih.GetAllIntegrationsDashboard)
 		clusterAPI.Get("/{"+utils.ClusterIdxPathParam+"}/integration/{"+utils.IntegrationIdxPathParam+"}/status", ih.TestIntegrationConnectionStatus)
+		clusterAPI.Post("/{"+utils.ClusterIdxPathParam+"}/integration/unsynced/status", ih.TestUnSyncedIntegrationConnection)
 
 		clusterAPI.Get("/attribute", ah.GetAttributes)
 		clusterAPI.Put("/attribute", ah.UploadAttributesCSV)
